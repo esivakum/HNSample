bd3d2d60f0962bb735db4a0cb3b8d91a2c19c6df
Ede Venkata Sivakumar <esivakum@in.ibm.com>
2020-06-25 10:23:35 +0530

    Comments removed.

--- HackersNews/hnapp/src/components/Details/Details.js
+++ HackersNews/hnapp/src/components/Details/Details.js
@@ -3,7 +3,6 @@ import DetailList from '../DetailList/DetailList';
 
 class Details extends Component {
     render() {// <h3>Details: {this.props.location.story.title}</h3>
-        // console.log(this.props.location.story);
         return (
             <div>
                 <DetailList story={this.props.location.story}></DetailList>
